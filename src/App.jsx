a1d318e19c87df180695837e6e821eedaedacd37
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-06-10 11:07:21 +0100

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import HeroSection from "./components/HeroSection";
+import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <main className="bg-black">Navbar Hero Section Highlights</main>;
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <HeroSection />
+      <Highlights />
+    </main>
+  );
 };
 
 export default App;
